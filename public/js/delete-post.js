517c316525ab0f021ef7baf796d54d6fc472125b
Fin M <qbres333@hotmail.com>
2024-10-07 11:27:44 -0600

    create edit/delete js files

---
+++ public/js/delete-post.js
@@ -0,0 +1,19 @@
+// deletion handler: send DELETE request to API if user clicks delete
+const deletePostHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blog/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete blog post');
+    }
+  }
+};
+
+// update bloglist on dashboard upon submit
+document.querySelector('.user-blog-list').addEventListener('submit', deletePostHandler);
--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,5 +1,21 @@
 
 // update handler: send PUT request to API if the user updates the post
+const updatePostHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
 
+        const response = await fetch(`/api/blog/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-type': 'application/json' },
+        })
 
-// deletion handler: send DELETE request to API if user clicks delete 
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to update blog post');
+        }
+    }
+}
+// update bloglist on dashboard upon submit
+document.querySelector('.user-blog-list').addEventListener('submit', updatePostHandler);
