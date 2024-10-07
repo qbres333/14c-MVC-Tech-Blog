74138bfbb257a3aacee88fe494f3b8c3bed26745
Fin M <qbres333@hotmail.com>
2024-10-07 10:43:57 -0600

    create edit-post handlebars, update css for update/delete buttons

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,3 +1,5 @@
-// retrieve post data when clicked
-// send PUT request to API if the user updates the post
-// send DELETE request to API if user clicks delete
+
+// update handler: send PUT request to API if the user updates the post
+
+
+// deletion handler: send DELETE request to API if user clicks delete 
