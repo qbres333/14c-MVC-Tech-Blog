876a3a5e3e924945951b5e87758539ac745bc239
Fin M <qbres333@hotmail.com>
2024-10-06 20:19:24 -0600

    add scripts to login/signup handlebars, create login.js

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // get values from the login inputs
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    // if both fields have values, send POST request to API endpoint
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // if request is successful, redirect to the user's dashboard
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    } else{
+        alert('Please enter both a username and password');
+    }
+}
